ead575fa1328ac30ca3b208a9b6713a9d0e6bcee
Jamie Geddes <jgeddes513@gmail.com>
2024-05-06 14:16:17 +0100

    small qol updates
    Cleaning up code little and updated z index on modal container for programmin stuff so shows better on phone did gis but forgot prior

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   content: ['index.html', 'functions.js','map.html','Unemployment_England_Regions.html'], // Add your HTML and JavaScript files
-  css: ['style.css','style2.css'], // Add your CSS files
+  css: ['cleaned-css\\style.css'], // Add your CSS files
 };
